2199a828038b26cf9f2122df4a37170c9acba1db
Harri Rautila <harri.rautila@gmail.com>
2011-12-09 23:47:51 +0200

    * Application setup() with new parameter config. * Some refactoring of code * Initial versions of running session in subprocess.

--- sxsuite/apps.py
+++ sxsuite/apps.py
@@ -92,6 +92,7 @@ class Application(Session):
     def downstream(self, data):
         raise ConfigError(self, S_ENOTDOWNMODULE)
 
+
 class ConsoleApplication(Application):
     """Simple session for reading and writing to console or terminal."""
 
@@ -112,74 +113,90 @@ class ConsoleApplication(Application):
 
 
 class Handler(object):
-    def setup(self, connection):
+    def setup(self, connection, config):
         pass
 
-    def handle(self, data):
+    def handle(self, data, connection):
         pass
 
-    def finish(self):
+    def finish(self, connection):
         pass
 
-    def __call__(self, data):
-        self.handle(data)
+    def __call__(self, data, connection):
+        self.handle(data, connection)
+
+
+class _Subprocess(object):
+    
+    def __init__(self, target, conn, config):
+        self.target = target
+        self.conn = conn
+        self.init = None
+        self.finish = None
+        self.running = False
+        self.config = config
+
+    def run(self):
+        """Run the application data receiving loop.
+
+        This is executed in the subprocess context.
+        """
+        signal.signal(signal.SIGTERM, self.sigterm)
+        logging.debug("starting subprocess in pid: %d [fd %d]",
+                      os.getpid(), self.conn.fileno())
+        # flush all timers as we are running in separate process
+        flush_timers()
+        os.closerange(3, self.conn.fileno())
+
+        self.init = getattr(self.target, 'setup', None)
+        self.finish = getattr(self.target, 'finish', None)
+        if hasattr(self.target, 'handle'):
+            self.handler = getattr(self.target, 'handle')
+        else:
+            self.handler = target
+        if not callable(self.handler):
+            raise ConfigError(self, exc.E_NOTCALLABLE)
+
+        if callable(self.init):
+            self.init(self.conn, self.config)
+        self.running = True
+        while self.running:
+            try:
+                readable = self.conn.poll(0.5)
+                run_timers()
+                if not readable:
+                    continue
+                data = self.conn.recv()
+            except (IOError, KeyboardInterrupt), ex:
+                break
+            except Exception, e:
+                logging.error("Subprocess recv failed: %s", str(e))
+                break
+            try:
+                result = self.handler(data, self.conn)
+                if result is not None:
+                    self.conn.send(result)
+            except Exception, e:
+                logging.error("Subprocess %s raised %s", str(self.target), str(e))
+                self.conn.send(e)
+        # end while
+        if callable(self.finish):
+            self.finish(self.conn)
+        self.conn.close()
+
+    def run_module(self, modulename):
+        import importlib
+        self.target = importlib.import_module(modulename)
+        self.run()
+
+    def sigterm(self, signum, frame):
+        logging.info("SIGTERM caught; terminating ...")
+        self.running = False
+
+
 
 class ProcessApplication(Application):
-
-    class Subprocess(object):
-        def __init__(self, target, conn):
-            self.target = target
-            self.conn = conn
-            self.init = None
-            self.finish = None
-            self.running = False
-            if hasattr(target, 'setup'):
-                self.init = getattr(target, 'setup')
-            if hasattr(target, 'finish'):
-                self.finish = getattr(target, 'finish')
-            if hasattr(target, 'handle'):
-                self.handler = getattr(target, 'handle')
-            else:
-                self.handler = target
-            if not callable(self.handler):
-                raise ConfigError(self, exc.E_NOTCALLABLE)
-
-        def run(self):
-            signal.signal(signal.SIGTERM, self.sigterm)
-            logging.debug("starting subprocess in pid: %d [fd %d]",
-                          os.getpid(), self.conn.fileno())
-            # flush all timers as we are running in separate process
-            flush_timers()
-            os.closerange(3, self.conn.fileno())
-            if callable(self.init):
-                self.init(self.conn)
-            self.running = True
-            while self.running:
-                try:
-                    readable = self.conn.poll(0.5)
-                    run_timers()
-                    if not readable:
-                        continue
-                    data = self.conn.recv()
-                except (IOError, KeyboardInterrupt), ex:
-                    break
-                except Exception, e:
-                    logging.error("Subprocess recv failed: %s", str(e))
-                    break
-                try:
-                    result = self.handler(data, self.conn)
-                    if result is not None:
-                        self.conn.send(result)
-                except Exception, e:
-                    logging.error("Subprocess %s raised %s", str(self.target), str(e))
-            # end while
-            if callable(self.finish):
-                self.finish(self.conn)
-            self.conn.close()
-
-        def sigterm(self, signum, frame):
-            logging.info("SIGTERM caught; terminating ...")
-            self.running = False
+    """Run application class in subprocess."""
 
     def __init__(self, reaktor, target, name=''):
         Application.__init__(self, None, name)
@@ -197,7 +214,7 @@ class ProcessApplication(Application):
         logging.debug("parent conn %d, child %d",
                       self.parent.fileno(), self.child.fileno())
         self.transport = ConnectedTransport(self._reaktor, self.parent, self)
-        self.runner = ProcessApplication.Subprocess(self.target, self.child)
+        self.runner = _Subprocess(self.target, self.child, self.config)
         self._process = Process(target=self.runner.run)
         self._process.start()
 
@@ -217,9 +234,14 @@ class ProcessApplication(Application):
         self._close(2.0)
 
     def send(self, data):
+        """Send data to application."""
         self.parent.send(data)
 
     def recv(self, data):
+        """Handle data received from subprocess application."""
+        if isinstance(data, Exception):
+            self.log.error("Subprocess exception: %s", str(data))
+            raise data
         self.received(data)
 
     def event_readable(self, transport):
@@ -234,3 +256,61 @@ class ProcessApplication(Application):
         if typ != KeyboardInterrupt:
             self.log.debug("process: %s", str(ex))
 
+
+class SessionProcess(object):
+
+    def __init__(self, sessionClass, protocol, config):
+        self.sessionClass = sessionClass
+        self.protocol = protocol
+        self.config = config
+
+    def start(self, netname, sslcontext=None):
+        self.parent, child = Pipe()
+        self.p = Process(target=run_session_process,
+                         args=(child, self.sessionClass, self.protocol, self.config, netname))
+        self.p.start()
+        
+    def close(self):
+        self.p.terminate()
+        self.p.join()
+
+
+def run_session_process(pipe, sessionClass, protocol, config, netaddr, sslcontext):
+    """Run session in separete sub process"""
+
+    from reaktor import Reaktor
+    from transport import PipeTransport
+
+    reaktor = Reaktor()
+    name = config.get('name', '_unnamed_')
+    if 'name' in config:
+        del config['name']
+
+    state_path = config.get('state_path', '')
+    if 'state_path' in config:
+        del config['state_path']
+    
+    msg_path = config.get('msg_store', '')
+    if 'msg_store' in config:
+        del config['msg_store']
+    
+    ses = sessionClass(reaktor, protocol, name=name, store_url=msg_path)
+
+    for key, val in config.items():
+        ses.set_conf(key, val)
+
+    pipe_transport = PipeTransport(pipe)
+    app = Application(transport=pipe_transport)
+    app.linkdown(ses)
+    ses.linkup(app)
+
+    if sslcontext is not None:
+        ses.ssl_context(sslcontext)
+
+    ses.start(netaddr)
+    app.start()
+    if state_path:
+        ses.state.restore(state_path)
+        savelist = [(ses, state_path)]
+    reaktor.run(savelist=savelist, exc_handler=exchandler)
+    
